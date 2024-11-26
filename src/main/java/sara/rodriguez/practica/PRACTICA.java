ab5f3cd05e6ce0c61cf882c568434d5a3176fd75
SaraRodriguez <sarayuley00@gmail.com>
2024-11-26 09:07:05 -0500

    hola

---
+++ src/main/java/sara/rodriguez/practica/PRACTICA.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package sara.rodriguez.practica;
+
+/**
+ *
+ * @author USER
+ */
+public class PRACTICA {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
